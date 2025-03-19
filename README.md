2dade1594fda8e6e53769905f872e284cbf10641
Preston Jones <psmak4@gmail.com>
2025-03-18 19:10:25 -0600

    Add Clerk for auth

--- README.md
+++ README.md
@@ -4,8 +4,10 @@
 - TypeScript
 - Vite
 - Prettier
+- Clerk
 
 ## Installation
 
 1. Clone the GitHub repo.
+1. Rename `.env.txt` to `.env`, and add missing values.
 1. Run `npm i`.
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+import { ClerkProvider } from '@clerk/clerk-react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/app.tsx'
 import './index.css'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+			<App />
+		</ClerkProvider>
 	</StrictMode>,
 )
